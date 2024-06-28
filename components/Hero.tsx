8799ded0096784ec9ef333ad92e30aca8357abef
sachithnim <nimendras07@gmail.com>
2024-06-29 00:01:47 +0530

    Add About section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -17,16 +17,16 @@ export const Hero = () => {
       <div className='flex justify-center relative my-20 z-10'>
         <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
           <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>
-            Dynamic web magic with Next.js
+            Web developer
           </h2>
           <TextGenerateEffect
 
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
-            words="Transforming Concepts into Seamless Experiences"
+            words="Eager to Apply the Latest Technologies in Solving Multifaceted Problems"
           />
 
-          <p>
-            
+          <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
+            Hi, I&apos;m Sachith Wasala, 
           </p>
 
         </div>
