a6c76ef86833a9230a7d6a616840b95cbc5daf9f
sachithnim <nimendras07@gmail.com>
2024-06-29 01:00:35 +0530

    Create Cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import { FaHome } from "react-icons/fa";
 
 
 export default function Home() {
@@ -6,7 +9,11 @@ export default function Home() {
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden
     mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+        <FloatingNav navItems={[
+          {name: 'Home', link: '/', icon: <FaHome />}
+        ]}/>
         <Hero />
+        <Grid />
       </div>
     </main>
   );
