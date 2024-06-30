2783fbc3c665b6e37719f4ef49f16cef20d49dcd
sachithnim <nimendras07@gmail.com>
2024-06-30 18:53:59 +0530

    Add details to 3rd Card

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,6 +1,11 @@
+'use client'
+
 import { cn } from "@/utils/cn";
 import { BackgroundGradientAnimation } from "./GradientBg";
 import { GlobeDemo } from "./GridGlobe";
+import { useState } from "react";
+import animationData from "@/data/confetti.json";
+import Lottie from "react-lottie";
 
 export const BentoGrid = ({
   className,
@@ -12,7 +17,7 @@ export const BentoGrid = ({
   return (
     <div
       className={cn(
-        "grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto ",
+        "grid grid-cols-1 md:grid-cols-6 lg:grid-cols-5 md:grid-row-7 gap-4 lg:gap-8 mx-auto",
         className
       )}
     >
@@ -40,10 +45,13 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
+  
+  const [copied, setCoppied] = useState(false);
+
   return (
     <div
       className={cn(
-        "row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
+        "row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4 border border-white/[0.1]",
         className
       )}
       style={{
@@ -62,7 +70,7 @@ export const BentoGridItem = ({
                     />
                 )}
             </div>
-            <div className={`absolute right-0 -bottom-5 ${id === 5 && "w-full opacity-80"} `}>
+            <div className={`absolute right-0 -bottom-5 ${id === 5 && "w-full opacity-80"}`}>
                 {spareImg && (
                     <img
                         src={spareImg}
@@ -73,24 +81,50 @@ export const BentoGridItem = ({
             </div>
             {id === 6 && (
                 <BackgroundGradientAnimation>
-                    <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl"></div>
+                    <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl"/>
                 </BackgroundGradientAnimation>
             )}
 
             <div className={cn(
                 titleClassName, 'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10'
             )}>
-                <div className="font-sans font-extralight text-[#c1c2d3]text-sm md:text-xs lg:text-base z-10">
+                <div className="font-sans font-extralight text-[#c1c2d3]text-sm md:text-xs">
                     {description}
                 </div>
                 <div className="font-sans font-bold text-lg lg:text-3xl max-w-96 z-10">
                      {title}
                 </div>
-            </div>
+            
             {id === 2 && <GlobeDemo />}
+
+            {id === 3 && (
+
+              <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
+                  <div className="flex flex-col gap-3 lg:gap-8">
+                    {['React.js', 'Next.js', 'TypeScript'].map((item) => (
+                      <span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
+                        {item}
+                      </span>
+                    ))}
+                    <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]"/>
+                  </div>
+
+                  <div className="flex flex-col gap-3 lg:gap-8">
+                  <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]"/>
+                    {['Python', 'MySQL', 'Kotlin'].map((item) => (
+                      <span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
+                        {item}
+                      </span>
+                    ))}
+              
+                  </div>      
+
+              </div>
+            )}
 
             
         </div>
+    </div>
     </div>
   );
 };
